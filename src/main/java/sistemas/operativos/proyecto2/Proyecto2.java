3b1f5b26566a809d1d4b01dffa4b1cc3069783bf
Sebas4624 <h.sebastian@correo.unimet.edu.ve>
2025-11-09 16:04:04 -0400

    commit inicial

---
+++ src/main/java/sistemas/operativos/proyecto2/Proyecto2.java
@@ -0,0 +1,12 @@
+package sistemas.operativos.proyecto2;
+
+/**
+ *
+ * @author sebas
+ */
+public class Proyecto2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
